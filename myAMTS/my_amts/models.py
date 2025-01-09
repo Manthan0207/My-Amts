dc30b10d18444a116fa54335a2ca5481bebd6af2
Satyam-Maru <satyammaru07@gmail.com>
2025-01-09 15:02:05 +0530

    migrations applied

--- myAMTS/my_amts/models.py
+++ myAMTS/my_amts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Bus(models.Model):
+    bus_number = models.CharField(max_length=10, unique=True)
+    
+    stops = models.JSONField()
+
+    def __str__(self):
+        return self.bus_number
